8d46791fd3397682c367fd1aad0911915db78bfa
AmirSaman Gharechahi <gharechahi72@gmail.com>
2025-07-19 18:39:04 +0330

    loading

---
+++ app/loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loading = () => {
+    return (
+        <div className="col-md-3 bg loading">
+            loading ...
+        </div>
+    );
+}
+
+export default Loading;
